16ae918a1d11a7b4bc1e803eaed670638ae35dff
Rahul balati <118408469+rahulbalati@users.noreply.github.com>
2023-10-29 05:52:29 +0530

    Update index.js

--- firstprogram/src/index.js
+++ firstprogram/src/index.js
@@ -4,3 +4,7 @@ import ReactDOM from 'react-dom/client';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <><h1>this is our first program</h1></>
 )
+/*Using pure Javascript*/
+var h1=document.createElement('h1');
+h1.innerHTML="Hi,there this rahul";
+document.getElementById("root").appendChild(h1); 
